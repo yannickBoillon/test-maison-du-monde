eaf4a48597689cc4a38a5c3e689ab428dd1525bd
yannick <yboillon@eleven-labs.com>
2018-05-15 13:18:46 +0200

    Create Category Entity

---
+++ symfony/src/Entity/Category.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class Category
+ *
+ * @ORM\Entity
+ * @ApiResource
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Name must not be null")
+     * @ORM\Column(type="string");
+     */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Category
+     */
+    public function setId(int $id): Category
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Category
+     */
+    public function setName(string $name): Category
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}

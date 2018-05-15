abfce717d603ef750de5f08c5b5fc5dcd1803585
yannick <yboillon@eleven-labs.com>
2018-05-15 13:37:12 +0200

    Create Product Entity

---
+++ symfony/src/Entity/Product.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class Product
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Name must not be blank")
+     * @ORM\Column(type="string");
+     */
+    private $name;
+
+    /**
+     * @var float
+     * @ORM\Column(type="float");
+     */
+    private $price;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string");
+     */
+    private $currency;
+
+    /**
+     * @var integer
+     * @ORM\Column(type="integer")
+     */
+    private $stock;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="App\Entity\Category")
+     *
+     */
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Product
+     */
+    public function setId(int $id): Product
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Product
+     */
+    public function setName(string $name): Product
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     * @return Product
+     */
+    public function setPrice(float $price): Product
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param string $currency
+     * @return Product
+     */
+    public function setCurrency(string $currency): Product
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStock(): int
+    {
+        return $this->stock;
+    }
+
+    /**
+     * @param int $stock
+     * @return Product
+     */
+    public function setStock(int $stock): Product
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getCategories(): ArrayCollection
+    {
+        return $this->categories;
+    }
+
+    /**
+     * @param ArrayCollection $categories
+     * @return Product
+     */
+    public function setCategories(ArrayCollection $categories): Product
+    {
+        $this->categories = $categories;
+        return $this;
+    }
+}
